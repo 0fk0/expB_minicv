d3491b17597cdac9feab964e2066570f0f387e2b
0fk0 <greeeen_ken41ei39jin@icloud.com>
2023-11-10 02:52:05 +0900

    feat:コード生成部のテストケース作成

--- src/main/java/lang/c/parse/Program.java
+++ src/main/java/lang/c/parse/Program.java
@@ -1,13 +1,14 @@
 package lang.c.parse;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 import lang.*;
 import lang.c.*;
 
 public class Program extends CParseRule {
 	// program ::= { statement } EOF
-	CParseRule program;
+	ArrayList<CParseRule> statementList = new ArrayList<CParseRule>();
 
 	public Program(CParseContext pcx) {
 	}
@@ -18,16 +19,15 @@ public class Program extends CParseRule {
 
 	public void parse(CParseContext pcx) throws FatalErrorException {
 		// ここにやってくるときは、必ずisFirst()が満たされている
-		CParseRule statement = null, list = null;
+		CParseRule statement = null;
 		CTokenizer ct = pcx.getTokenizer();
 		CToken tk = ct.getCurrentToken(pcx);
 		while (Statement.isFirst(tk)) {
-			list = new Statement(pcx);
-			list.parse(pcx);
-			statement = list;
+			statement = new Statement(pcx);
+			statement.parse(pcx);
+			statementList.add(statement);
 			tk = ct.getCurrentToken(pcx);
 		}
-		program = statement;
 
 		if (tk.getType() != CToken.TK_EOF) {
 			pcx.fatalError(tk.toExplainString() + "プログラムの最後にゴミがあります");
@@ -35,8 +35,10 @@ public class Program extends CParseRule {
 	}
 
 	public void semanticCheck(CParseContext pcx) throws FatalErrorException {
-		if (program != null) {
-			program.semanticCheck(pcx);
+		if (!statementList.isEmpty()) {
+			for (CParseRule statement : statementList) {
+				statement.semanticCheck(pcx);
+			}
 		}
 	}
 
@@ -45,12 +47,15 @@ public class Program extends CParseRule {
 		o.println(";;; program starts");
 		o.println("\t. = 0x100");
 		o.println("\tJMP\t__START\t; ProgramNode: 最初の実行文へ");
-		// ここには将来、宣言に対するコード生成が必要
-		if (program != null) {
-			o.println("__START:");
-			o.println("\tMOV\t#0x1000, R6\t; ProgramNode: 計算用スタック初期化");
-			program.codeGen(pcx);
-			// o.println("\tMOV\t-(R6), R0\t; ProgramNode: 計算結果確認用");
+		// (ここには将来、宣言に対するコード生成が必要)
+		o.println("__START:");
+		o.println("\tMOV\t#0x1000, R6\t; ProgramNode: 計算用スタック初期化");
+		// o.println("\tMOV\t-(R6), R0\t; ProgramNode: 計算結果確認用"); // 代入分が完成したため不要
+
+		if (!statementList.isEmpty()) {
+			for (CParseRule statement : statementList) {
+				statement.codeGen(pcx);
+			}
 		}
 		o.println("\tHLT\t\t\t; ProgramNode:");
 		o.println("\t.END\t\t\t; ProgramNode:");
--- src/test/java/lang/c/parse/CodeGenStatementAssignTest.java
+++ src/test/java/lang/c/parse/CodeGenStatementAssignTest.java
@@ -48,15 +48,13 @@ public class CodeGenStatementAssignTest {
     public void assign() throws FatalErrorException {
         inputStream.setInputString("i_A = i_B;");
         String expected[] = {
-            "Write",
-            "down",
-            "the",
-            "output",
-            "you",
-            "have",
-            "decided",
-            "on",
-            "here"
+            "   MOV #i_A, (R6)+     ; Ident: 変数アドレスを積む",
+            "   MOV #i_B, (R6)+     ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; addressToValue: アドレスを取り出して、内容を参照して、積む",
+            "   MOV (R0), (R6)+     ; addressToValue:",
+            "   MOV -(R6), R0       ; statementAssign: 左辺の変数アドレスと右辺の値を取り出して、右辺の値を左辺の変数アドレスに代入",
+            "   MOV -(R6), R1       ; statementAssign:",
+            "   MOV R0, (R1)        ; statementAssign:"
         };
 
         // Check only code portion, not validate comments
@@ -69,15 +67,11 @@ public class CodeGenStatementAssignTest {
     public void assignInt() throws FatalErrorException {
         inputStream.setInputString("i_a=0;");
         String expected[] = {
-            "Write",
-            "down",
-            "the",
-            "output",
-            "you",
-            "have",
-            "decided",
-            "on",
-            "here"
+            "   MOV #i_a, (R6)+     ; Ident: 変数アドレスを積む",
+            "   MOV #0, (R6)+       ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; statementAssign: 左辺の変数アドレスと右辺の値を取り出して、右辺の値を左辺の変数アドレスに代入",
+            "   MOV -(R6), R1       ; statementAssign:",
+            "   MOV R0, (R1)        ; statementAssign:"
         };
 
         // Check only code portion, not validate comments
@@ -87,21 +81,84 @@ public class CodeGenStatementAssignTest {
 
     // (2) ポインタ型の扱い
     // Please copy and paste the above and add the specified test case to the following
+    @Test
+    public void assignPint1() throws FatalErrorException {
+        inputStream.setInputString("ip_a=&1;");
+        String expected[] = {
+            "   MOV #ip_a, (R6)+     ; Ident: 変数アドレスを積む",
+            "   MOV #1, (R6)+       ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; statementAssign: 左辺の変数アドレスと右辺の値を取り出して、右辺の値を左辺の変数アドレスに代入",
+            "   MOV -(R6), R1       ; statementAssign:",
+            "   MOV R0, (R1)        ; statementAssign:"
+        };
+
+        // Check only code portion, not validate comments
+        CParseRule rule = new StatementAssign(cpContext);
+        helper.checkCodeGen(expected, rule, cpContext);
+    }
+
+    @Test
+    public void assignPint2() throws FatalErrorException {
+        inputStream.setInputString("*ip_a=1;");
+        String expected[] = {
+            "	MOV	#ip_a, (R6)+    ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; PrimaryMult: アドレスを取り出して、内容を参照して、積む",
+			"   MOV (R0), (R6)+     ; PrimaryMult:",
+            "   MOV #1, (R6)+       ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; statementAssign: 左辺の変数アドレスと右辺の値を取り出して、右辺の値を左辺の変数アドレスに代入",
+            "   MOV -(R6), R1       ; statementAssign:",
+            "   MOV R0, (R1)        ; statementAssign:"
+        };
+
+        // Check only code portion, not validate comments
+        CParseRule rule = new StatementAssign(cpContext);
+        helper.checkCodeGen(expected, rule, cpContext);
+    }
+
+    @Test
+    public void assignPint3() throws FatalErrorException {
+        inputStream.setInputString("ip_a=&1;*ip_a=1;");
+        String expected[] = {
+            "   . = 0x100",
+            "   JMP __START            ; ProgramNode: 最初の実行文へ",
+            "__START:",
+		    "   MOV #0x1000, R6     ; ProgramNode: 計算用スタック初期化",
+            "   MOV #ip_a, (R6)+    ; Ident: 変数アドレスを積む",
+            "   MOV #1, (R6)+       ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; statementAssign: 左辺の変数アドレスと右辺の値を取り出して、右辺の値を左辺の変数アドレスに代入",
+            "   MOV -(R6), R1       ; statementAssign:",
+            "   MOV R0, (R1)        ; statementAssign:",
+            "	MOV	#ip_a, (R6)+    ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; PrimaryMult: アドレスを取り出して、内容を参照して、積む",
+			"   MOV (R0), (R6)+     ; PrimaryMult:",
+            "   MOV #1, (R6)+       ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; statementAssign: 左辺の変数アドレスと右辺の値を取り出して、右辺の値を左辺の変数アドレスに代入",
+            "   MOV -(R6), R1       ; statementAssign:",
+            "   MOV R0, (R1)        ; statementAssign:",
+            "   HLT                 ; ProgramNode:",
+		    "   .END                ; ProgramNode:"
+        };
+
+        // Check only code portion, not validate comments
+        CParseRule rule = new Program(cpContext);
+        helper.checkCodeGen(expected, rule, cpContext);
+    }
 
     // (3) 配列型の扱い
     @Test
     public void assignArray() throws FatalErrorException {
         inputStream.setInputString("ia_a[3]=1;");
         String expected[] = {
-            "Write",
-            "down",
-            "the",
-            "output",
-            "you",
-            "have",
-            "decided",
-            "on",
-            "here"
+            "	MOV	#ia_a, (R6)+    ; Ident: 変数アドレスを積む",
+            "	MOV	#3, (R6)+       ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; Variable: 配列名とindexを取り出して配列先頭アドレスとindex分を足し、内容を参照して、積む",
+        	"   MOV -(R6), R1       ; Variable:",
+			"   ADD R0, R1          ; Variable:",
+			"   MOV R1, (R6)+       ; Variable",
+            "   MOV #1, (R6)+       ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; statementAssign: 左辺の変数アドレスと右辺の値を取り出して、右辺の値を左辺の変数アドレスに代入",
+            "   MOV -(R6), R1       ; statementAssign:",
+            "   MOV R0, (R1)        ; statementAssign:"
         };
 
         // Check only code portion, not validate comments
@@ -111,5 +168,47 @@ public class CodeGenStatementAssignTest {
 
     // (4) ポインタ配列型の扱い
     // Please copy and paste the above code and add the specified test case to the following
+    @Test
+    public void assignPintArray1() throws FatalErrorException {
+        inputStream.setInputString("ipa_a[3]=&3;");
+        String expected[] = {
+            "	MOV	#ipa_a, (R6)+    ; Ident: 変数アドレスを積む",
+            "	MOV	#3, (R6)+       ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; Variable: 配列名とindexを取り出して配列先頭アドレスとindex分を足し、内容を参照して、積む",
+        	"   MOV -(R6), R1       ; Variable:",
+			"   ADD R0, R1          ; Variable:",
+			"   MOV R1, (R6)+       ; Variable",
+            "   MOV #3, (R6)+       ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; statementAssign: 左辺の変数アドレスと右辺の値を取り出して、右辺の値を左辺の変数アドレスに代入",
+            "   MOV -(R6), R1       ; statementAssign:",
+            "   MOV R0, (R1)        ; statementAssign:"
+        };
 
+        // Check only code portion, not validate comments
+        CParseRule rule = new StatementAssign(cpContext);
+        helper.checkCodeGen(expected, rule, cpContext);
+    }
+
+    @Test
+    public void assignPintArray2() throws FatalErrorException {
+        inputStream.setInputString("*ipa_a[3]=3;");
+        String expected[] = {
+            "	MOV	#ipa_a, (R6)+    ; Ident: 変数アドレスを積む",
+            "	MOV	#3, (R6)+       ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; Variable: 配列名とindexを取り出して配列先頭アドレスとindex分を足し、内容を参照して、積む",
+        	"   MOV -(R6), R1       ; Variable:",
+			"   ADD R0, R1          ; Variable:",
+			"   MOV R1, (R6)+       ; Variable",
+            "   MOV -(R6), R0       ; PrimaryMult: アドレスを取り出して、内容を参照して、積む",
+			"   MOV (R0), (R6)+     ; PrimaryMult:",
+            "   MOV #3, (R6)+       ; Ident: 変数アドレスを積む",
+            "   MOV -(R6), R0       ; statementAssign: 左辺の変数アドレスと右辺の値を取り出して、右辺の値を左辺の変数アドレスに代入",
+            "   MOV -(R6), R1       ; statementAssign:",
+            "   MOV R0, (R1)        ; statementAssign:"
+        };
+
+        // Check only code portion, not validate comments
+        CParseRule rule = new StatementAssign(cpContext);
+        helper.checkCodeGen(expected, rule, cpContext);
+    }
 }
