8a8a4f1e00ff8f69f440cc8aaa47d5447198037c
0fk0 <greeeen_ken41ei39jin@icloud.com>
2023-11-29 00:52:55 +0900

    feat:input/outputも構文解析・意味解析部分を追加実装

--- src/main/java/lang/c/parse/StatementInput.java
+++ src/main/java/lang/c/parse/StatementInput.java
@@ -6,66 +6,42 @@ import lang.*;
 import lang.c.*;
 
 public class StatementInput extends CParseRule {
-	// statementInput ::= primary ASSIGN expression SEMI
-	CParseRule primary, expression;
-	CToken assign, semi;
+	// statementInput ::= INPUT num
+	CParseRule number;
+	CToken input;
 
 	public StatementInput(CParseContext pcx) {
 	}
 
 	public static boolean isFirst(CToken tk) {
-		return (Primary.isFirst(tk));
+		return (tk.getType() == CToken.TK_INPUT);
 	}
 
 	public void parse(CParseContext pcx) throws FatalErrorException {
 		CTokenizer ct = pcx.getTokenizer();
 		CToken tk = ct.getCurrentToken(pcx);
-		if (Primary.isFirst(tk)){
-			primary = new Primary(pcx);
-			primary.parse(pcx);
-		}
-		tk = ct.getCurrentToken(pcx);
-		if (tk.getType() == CToken.TK_ASSIGN) {
-			assign = tk;
-		} else {
-			pcx.fatalError(tk.toExplainString() + "変数の後には=が必要です");
-		}
+		input = tk;
+
 		tk = ct.getNextToken(pcx);
-		if (Expression.isFirst(tk)) {
-			expression = new Expression(pcx);
-			expression.parse(pcx);
+		if (Number.isFirst(tk)) {
+			number = new Number(pcx);
+			number.parse(pcx);
 		} else {
-			pcx.fatalError(tk.toExplainString() + "=の後ろはexpressionです");
+			pcx.fatalError(tk.toExplainString() + "INPUTの後ろにはnumberが必要です");
 		}
-		tk = ct.getCurrentToken(pcx);
-		if (tk.getType() == CToken.TK_SEMI) {
-			semi = tk;
-		} else {
-			pcx.fatalError(tk.toExplainString() + "代入文の最後には;が必要です");
-		}
-		tk = ct.getNextToken(pcx);
 	}
 
 	public void semanticCheck(CParseContext pcx) throws FatalErrorException {
-		if (primary != null && expression != null){
-			primary.semanticCheck(pcx);
-			expression.semanticCheck(pcx);
-
-			if (primary.getCType() != expression.getCType()){
-				pcx.fatalError(assign.toExplainString() + "左辺の型[" + primary.getCType().toString() + "]と右辺の型[" + expression.getCType().toString() + "]が一致しません");
-			}
-			if (primary.isConstant()){
-				pcx.fatalError(assign.toExplainString() + "左辺が定数で代入できません");
-			}
+		if (input != null && number != null){
+			number.semanticCheck(pcx);
 		}
 	}
 
 	public void codeGen(CParseContext pcx) throws FatalErrorException {
 		PrintStream o = pcx.getIOContext().getOutStream();
 		o.println(";;; statementInput starts");
-		if (primary != null && expression != null) {
-			primary.codeGen(pcx);
-			expression.codeGen(pcx);
+		if (input != null && number != null) {
+			number.codeGen(pcx);
 
 			o.println("\tMOV\t-(R6), R0\t; statementInput: 左辺の変数アドレスと右辺の値を取り出して、右辺の値を左辺の変数アドレスに代入");
 			o.println("\tMOV\t-(R6), R1\t; statementInput:");
--- src/main/java/lang/c/parse/StatementOutput.java
+++ src/main/java/lang/c/parse/StatementOutput.java
@@ -6,66 +6,42 @@ import lang.*;
 import lang.c.*;
 
 public class StatementOutput extends CParseRule {
-	// statementOutput ::= primary ASSIGN expression SEMI
-	CParseRule primary, expression;
-	CToken assign, semi;
+	// statementInput ::= OUTPUT num
+	CParseRule number;
+	CToken output;
 
 	public StatementOutput(CParseContext pcx) {
 	}
 
 	public static boolean isFirst(CToken tk) {
-		return (Primary.isFirst(tk));
+		return (tk.getType() == CToken.TK_OUTPUT);
 	}
 
 	public void parse(CParseContext pcx) throws FatalErrorException {
 		CTokenizer ct = pcx.getTokenizer();
 		CToken tk = ct.getCurrentToken(pcx);
-		if (Primary.isFirst(tk)){
-			primary = new Primary(pcx);
-			primary.parse(pcx);
-		}
-		tk = ct.getCurrentToken(pcx);
-		if (tk.getType() == CToken.TK_ASSIGN) {
-			assign = tk;
-		} else {
-			pcx.fatalError(tk.toExplainString() + "変数の後には=が必要です");
-		}
+		output = tk;
+
 		tk = ct.getNextToken(pcx);
-		if (Expression.isFirst(tk)) {
-			expression = new Expression(pcx);
-			expression.parse(pcx);
+		if (Number.isFirst(tk)) {
+			number = new Number(pcx);
+			number.parse(pcx);
 		} else {
-			pcx.fatalError(tk.toExplainString() + "=の後ろはexpressionです");
+			pcx.fatalError(tk.toExplainString() + "OUTPUTの後ろにはnumberが必要です");
 		}
-		tk = ct.getCurrentToken(pcx);
-		if (tk.getType() == CToken.TK_SEMI) {
-			semi = tk;
-		} else {
-			pcx.fatalError(tk.toExplainString() + "代入文の最後には;が必要です");
-		}
-		tk = ct.getNextToken(pcx);
 	}
 
 	public void semanticCheck(CParseContext pcx) throws FatalErrorException {
-		if (primary != null && expression != null){
-			primary.semanticCheck(pcx);
-			expression.semanticCheck(pcx);
-
-			if (primary.getCType() != expression.getCType()){
-				pcx.fatalError(assign.toExplainString() + "左辺の型[" + primary.getCType().toString() + "]と右辺の型[" + expression.getCType().toString() + "]が一致しません");
-			}
-			if (primary.isConstant()){
-				pcx.fatalError(assign.toExplainString() + "左辺が定数で代入できません");
-			}
+		if (output != null && number != null){
+			number.semanticCheck(pcx);
 		}
 	}
 
 	public void codeGen(CParseContext pcx) throws FatalErrorException {
 		PrintStream o = pcx.getIOContext().getOutStream();
 		o.println(";;; statementOutput starts");
-		if (primary != null && expression != null) {
-			primary.codeGen(pcx);
-			expression.codeGen(pcx);
+		if (output != null && number != null) {
+			number.codeGen(pcx);
 
 			o.println("\tMOV\t-(R6), R0\t; statementOutput: 左辺の変数アドレスと右辺の値を取り出して、右辺の値を左辺の変数アドレスに代入");
 			o.println("\tMOV\t-(R6), R1\t; statementOutput:");
